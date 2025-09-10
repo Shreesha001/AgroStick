ccdc26b8a3d44d366aa54361d821f6593f128377
shreesha <shettyshreesha552@gmail.com>
2025-09-10 16:41:10 +0530

    Add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:agro_stick/splash_screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';  // Import the generated file
+import 'firebase_options.dart'; // Import the generated file 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,18 +10,21 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Agrostick App',
-      home: Scaffold(
-        appBar: AppBar(title: Text('Firebase Initialized')),
-        body: Center(child: Text('Welcome to Agrostick!')),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
       ),
+      home: const SplashScreen(), // Set SplashScreen as the initial screen
     );
   }
 }
--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryGreen = Color(0xFF2E7D32); // Dark green for gradient start
+  static const Color deepGreen = Color(0xFF1B5E20); // Dark green for gradient start
   static const Color lightGreen = Color(0xFFA5D6A7); // Light green for gradient end
-  static const Color textPrimary = Color(0xFFFFFFFF); // White for main text
-  static const Color textSecondary = Color(0xFFB0BEC5); // Light gray for tagline
+  static const Color goldenAccent = Color(0xFFFFCA28); // Gold for loader
+  static const Color textPrimary = Color(0xFFFFFFFF); // White for text
+  static const Color shadowColor = Color(0xFF000000); // Black for subtle shadow
 }
